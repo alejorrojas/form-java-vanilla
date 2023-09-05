36fd466d1188f8c6895c12a40857eb0346301f45
Alejo <alejoivanrojas@gmail.com>
2023-09-05 00:35:22 -0300

    Add GUI logic - modify UI 💅

--- src/main/java/logica/Agenda.java
+++ src/main/java/logica/Agenda.java
@@ -1,11 +1,17 @@
 
 package logica;
 
+import gui.Principal;
+
 
 
 public class Agenda {
 
     public static void main(String[] args) {
+        
+        Principal screen = new Principal();
+        screen.setVisible(true);
+        screen.setLocationRelativeTo(null);
         
         
         
